27f74aa7dc4c05949944ba43af571763a227665d
Iamneo <techadmin@iamneo.ai>
2023-08-24 06:52:39 +0000

    Backup Commit

--- demo/src/main/java/com/Task.java
+++ demo/src/main/java/com/Task.java
@@ -1,4 +1,15 @@
+package com.companyname.springbootcrudrest.model
+import java.util.Date;
+
+import jakarta.persistence.*;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Entity
+@Table(name="task")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
