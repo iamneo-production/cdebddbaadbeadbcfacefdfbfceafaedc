caeea7d72f50d96b512239cada043cba7ae109b5
Iamneo <techadmin@iamneo.ai>
2023-08-24 08:21:49 +0000

    Backup Commit

--- demo/src/main/java/com/example/demo/controller/TaskController.java
+++ demo/src/main/java/com/example/demo/controller/TaskController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -17,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo;
+import com.example.demo.model;
+import com.example.demo.repository;
 
 @RestController
 public class TaskController {
