198e3676cdbbf3cf1e570f1745aca566852bf68e
Iamneo <techadmin@iamneo.ai>
2023-08-24 12:06:26 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TaskController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.examly.springapp.model with tasks;
+import com.examly.springapp.model;
 import com.examly.springapp.repository;
 
 @RestController
