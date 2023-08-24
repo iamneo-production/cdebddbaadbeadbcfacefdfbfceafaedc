6f236a73c834d841875ba305adb5aa6ea60a3b64
Iamneo <techadmin@iamneo.ai>
2023-08-24 07:02:35 +0000

    Backup Commit

--- demo/src/main/java/com/example/demo/TaskRepository.java
+++ demo/src/main/java/com/example/demo/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo;
 public interface TaskRepository extends JpaRepository<Task, Long> {
     // Define custom queries if needed
 }
